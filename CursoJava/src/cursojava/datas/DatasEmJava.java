b19988842db1964e8bb74a2e7f7434a873934a93
ruvia <ruvia@DESKTOP-95IF2PH>
2021-10-21 01:18:21 -0300

    Trabalhando com datas

---
+++ CursoJava/src/cursojava/datas/DatasEmJava.java
@@ -0,0 +1,16 @@
+package cursojava.datas;
+
+import java.util.Date;
+
+public class DatasEmJava {
+	public static void main(String[] args) {
+		Date date = new Date();
+		System.out.println("Data em milisegundos: "+date.getTime());
+		System.out.println("Dia do mês: "+date.getDate());
+		System.out.println("Dia da semana: "+date.getDay());
+		System.out.println("Hora: "+date.getHours());
+		System.out.println("Minutos: "+date.getMinutes());
+		System.out.println("Segundos: "+date.getSeconds());
+		System.out.println("Ano mais 1900: "+(date.getYear()+1900));
+	}
+}
