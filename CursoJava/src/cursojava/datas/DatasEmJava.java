14aeb9616ebb3f39910d56d121092387ea18bb02
ruvia <ruvia@DESKTOP-95IF2PH>
2021-10-25 22:49:07 -0300

    Trabalhando com datas

--- CursoJava/src/cursojava/datas/DatasEmJava.java
+++ CursoJava/src/cursojava/datas/DatasEmJava.java
@@ -1,16 +1,77 @@
 package cursojava.datas;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DatasEmJava {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		
+		//After e before
+		SimpleDateFormat simpleDateFormatAB = new SimpleDateFormat("dd/MM/yyyy");
+		Date dataVencimentoBoleto =simpleDateFormatAB.parse("26/10/2021");
+		
+		Date dataAtualHoje = simpleDateFormatAB.parse("26/10/2021");
+		
+		//Passou da data Atual
+		if(dataVencimentoBoleto.after(dataAtualHoje)) {
+			System.out.println("After: Boleto dentro do prazo!!!\n\n");
+		}else {
+			System.out.println("After: Boleto Vencido!!!\n\n");
+		}
+		
+		//É anterior a data Atual
+		if(dataVencimentoBoleto.before(dataAtualHoje)) {
+			System.out.println("Before: Boleto Vencido!!!\n\n");
+		}else {
+			System.out.println("Before: Boleto dentro do prazo!!!\n\n");
+			
+		}
+		//-----------------------------------------------
+		Calendar calendar = Calendar.getInstance();
+		//Prefira usar o calendar
+		System.out.println("Calendar em milisegundos: "+calendar.getTimeInMillis());
+		System.out.println("Dia do mês: "+calendar.get(calendar.DAY_OF_MONTH));
+		System.out.println("Dia da semana: "+(calendar.get(calendar.DAY_OF_WEEK)-1));
+		System.out.println("Hora do Dia: "+calendar.get(calendar.HOUR_OF_DAY));
+		System.out.println("Minuto: "+calendar.get(calendar.MINUTE));
+		System.out.println("Segundos: "+calendar.get(calendar.SECOND));
+		System.out.println("Calendar Ano: "+calendar.get(calendar.YEAR));
+		
+		System.out.println("Calendar Data Atual: "+ calendar.getTime());//basta passar para o simple date format para formatar
+		
+		/*--------- Date ---------------------------------------------*/
 		Date date = new Date();
-		System.out.println("Data em milisegundos: "+date.getTime());
+		System.out.println("\nData em milisegundos: "+date.getTime());
 		System.out.println("Dia do mês: "+date.getDate());
 		System.out.println("Dia da semana: "+date.getDay());
 		System.out.println("Hora: "+date.getHours());
 		System.out.println("Minutos: "+date.getMinutes());
 		System.out.println("Segundos: "+date.getSeconds());
-		System.out.println("Ano mais 1900: "+(date.getYear()+1900));
+		System.out.println("Ano mais 1900: "+(date.getYear()+1900)+"\n");
+		
+		/*--------------------- Simple Date Format -------------------*/
+		//SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");//Salvar no banco de dados
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss:SS");//Mostrar para o Usuario
+		System.out.println(simpleDateFormat.format(date));
+		
+		System.out.println(simpleDateFormat.parse("11/03/1988 17:32:21:457"));//tem que ser de acordo com o ultimo formato instanciado, converte para um objeto data
+		
+		
+		//--------------------------------------------------------------------------------
+		System.out.println("\n\n.................Calculo de dia, ano, mês com Calendar ...............");
+		Calendar cal = Calendar.getInstance();//Pega a data Atual
+		//Simular que a data vem do banco de dados		
+		cal.setTime(new SimpleDateFormat("dd-MM-yyyy").parse("10-03-2021"));
+		cal.add(Calendar.DAY_OF_MONTH, 5);//data de hoje mais 5 dias, negativo diminui
+		System.out.println("Somando o dia: "+new SimpleDateFormat("dd-MM-yyyy").format(cal.getTime()));
+		
+		cal.add(Calendar.MONTH, 1);//data de hoje mais 1 mês
+		System.out.println("Somando o mês: "+new SimpleDateFormat("dd-MM-yyyy").format(cal.getTime()));
+		
+		cal.add(Calendar.YEAR, 1);//data de hoje mais 1 ano
+		System.out.println("Somando o ano: "+new SimpleDateFormat("dd-MM-yyyy").format(cal.getTime()));
+		
 	}
 }
